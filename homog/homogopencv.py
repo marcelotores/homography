1601f84093b7ad8b6612e376b2817461f4d10f85
marcelotores <marcelotores21@gmail.com>
2023-03-29 12:22:48 -0300

    prod imagens

--- homog/homogopencv.py
+++ homog/homogopencv.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
 
-im_src = cv2.imread('../imagens/1_red.jpg')
+im_src = cv2.imread('../imagens/2_red.jpg')
 
-pts_src = np.array([365, 189,
-                    164, 321,
-                    609, 572,
-                    676, 235]).reshape((4, 2))
+## imagem 2
+pts_src = np.array([208, 313,
+                393, 558,
+                691, 288,
+                454, 222,]).reshape((4, 2))
 
-pts_dst = np.array([137, 264,
-                    99, 560,
-                    613, 570,
-                    587, 215]).reshape((4, 2))
+## imagem 2
+pts_dst = np.array([194, 235,
+                192, 534,
+                696, 523,
+                729, 209,]).reshape((4, 2))
 
 # Calculate Homography
 h, status = cv2.findHomography(pts_src, pts_dst)
@@ -20,5 +22,7 @@ h, status = cv2.findHomography(pts_src, pts_dst)
 im_out = cv2.warpPerspective(im_src, h, (im_src.shape[1], im_src.shape[0]))
 
 cv2.imshow("findHomo", im_out)
+
+cv2.imwrite('../imagens/2_red_h.jpg', im_out)
 
 cv2.waitKey(0)
